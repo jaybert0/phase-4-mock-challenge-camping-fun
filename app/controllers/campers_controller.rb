f817acdb4d88640657fc5493fae284d7454a00a5
J Lee <jay.j.lee.1@gmail.com>
2022-03-02 17:33:46 -0500

    commit tree 53b91ceb01ca321d86fb0c5182c3f1407c45d528

---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,21 @@
+class CampersController < ApplicationController
+
+    def index
+        render json: Camper.all, status: :ok
+    end
+
+    def show
+        camper = Camper.find(params[:id])
+        render json: camper, serializer: CamperShowSerializer, status: :ok
+    end
+
+    def create
+        camper = Camper.create!(camper_params)
+        render json: camper, status: :created
+    end
+
+    private
+    def camper_params
+        params.permit(:name, :age)
+    end
+end
